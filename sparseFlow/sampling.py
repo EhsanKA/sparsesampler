6765f8bdb6c8c9f36bff5b7ff0e1f54ef035a75f
EhsanKA <e.karimiara@gmail.com>
2024-10-31 13:32:35 +0100

    description added

--- sparseFlow/sampling.py
+++ sparseFlow/sampling.py
@@ -6,16 +6,46 @@ import pandas as pd
 from sparseFlow.preprocessing import perform_pca_binning, adjust_feature_importances, accumulate_indices_until_threshold
 
 
-def sample(adata, size, seed=1234):
+def sample(adata=None, X=None, size=50000, seed=1234):
+    """
+    Perform PCA and binning to sample cells based on the PCA space.
+    Parameters
+    ----------
+    adata: AnnData
+        Annotated data matrix.
+    X: np.ndarray
+        Data matrix.
+    size: int
+        Number of cells to sample.
+    seed: int
+        Random seed.
+    Returns
+    -------
+    samples: list
+        List of indices of sampled cells.
+    elapsed_time: float
+        Elapsed time in seconds.
+    Raises
+    ------
+    ValueError
+        Either adata or X must be provided. If both are provided, adata will be used.
+    """
+
     scaler = StandardScaler()
-    data_standardized = scaler.fit_transform(adata.X)
+    if adata is not None:
+        data_standardized = scaler.fit_transform(adata.X)
+    elif X is not None:
+        data_standardized = scaler.fit_transform(X)
+    else:
+        raise ValueError("Either adata or X must be provided.")
+
     X = data_standardized
 
     random.seed(seed)
     print(f'********* #Start# *********')
     start_time = time.time()
 
-    n_components = min(adata.shape[1], 100)
+    n_components = min(X.shape[1], 100)
     pca = PCA(n_components=n_components)
     pca.fit(X)
     X_pca = pca.transform(X)
